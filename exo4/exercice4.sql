9a180ad91e139f4a911f3e96993a24da08fffb0e
Anthony Le Play <anthony.leplay@gmail.com>
2020-07-24 12:23:28 +0200

    petite modif

--- exo4/exercice4.sql
+++ exo4/exercice4.sql
@@ -4,29 +4,29 @@
 SELECT `studiant`.`firstname_studiant`,`studiant`.`lastname_studiant`,`note`.`value_note`,`subject`.`name_subject`
 FROM `note`
 
-INNER JOIN `studiant` 
+LEFT JOIN `studiant` 
 ON `note`.`id_studiant` = `studiant`.`id_studiant`
 
-INNER JOIN `subject` 
+LEFT JOIN `subject` 
 ON `note`.`id_subject` = `subject`.`id_subject`
 
 
 
 -- Il faudra pouvoir afficher la moyenne générale de chaque élève 
 -- (nom et prénom de l'élève, la classe de l'élève, le nom du professeur principal, la moyenne)
-SELECT `studiant`.`firstname_studiant`,`studiant`.`lastname_studiant`,`classroom`.`name_classroom`,`teacher`.`lastname_teacher`,AVG(`note`.`value_note`)
+SELECT `studiant`.`firstname_studiant`,`studiant`.`lastname_studiant`,`classroom`.`name_classroom`,`teacher`.`lastname_teacher`,ROUND(AVG(`note`.`value_note`),2) AS `moyenne`
 FROM `studiant`
 
-INNER JOIN `note` 
+LEFT JOIN `note` 
 ON `studiant`.`id_studiant` = `note`.`id_studiant`
 
-INNER JOIN `teacher` 
+LEFT JOIN `teacher` 
 ON `studiant`.`id_teacher` = `teacher`.`id_teacher`
 
-INNER JOIN `classroom` 
+LEFT JOIN `classroom` 
 ON `studiant`.`id_classroom` = `classroom`.`id_classroom`
 
-GROUP BY `studiant`.`firstname_studiant`
+GROUP BY `studiant`.`id_studiant`
 
 
 
